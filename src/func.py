6f697b1128d4d65e57d089406046436f7f8170af
Yose Sotomayor <120493435+Yosesotomayor@users.noreply.github.com>
2026-02-18 18:00:42 -0600

    feature: func.py

---
+++ src/func.py
@@ -0,0 +1,2 @@
+def func():
+      return "Hola mundo!"
---
+++ tests/test_func.py
@@ -0,0 +1,6 @@
+import pytest
+from src.func import func
+
+def test_apply_business_mapping():
+      result = func()
+      assert result == "Hola mundo!"
