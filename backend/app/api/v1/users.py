62e877e6317341d78337a4cdccb60c7088f3c0c7
bruno-rda <206255519+bruno-rda@users.noreply.github.com>
2026-03-12 13:26:39 -0600

    fix: make user id into string for comparison to block self deletion or update

--- backend/app/api/v1/users.py
+++ backend/app/api/v1/users.py
@@ -36,7 +36,7 @@ async def update_user(
     auth_service: AuthService = Depends(get_auth_service),
     current_user: User = Depends(get_current_user),
 ):
-    if current_user.id == user_id:
+    if str(current_user.id) == user_id:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="You cannot update yourself"
@@ -52,7 +52,7 @@ async def delete_user(
     auth_service: AuthService = Depends(get_auth_service),
     current_user: User = Depends(get_current_user),
 ):
-    if current_user.id == user_id:
+    if str(current_user.id) == user_id:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="You cannot delete yourself"
